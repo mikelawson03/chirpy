3db89e8a319edb9f57aa94be1637e76d4d672aa7
Mike Lawson <mikelawson03@gmail.com>
2026-05-03 14:25:33 -0400

    add handlers for /app/, /healthz, /metrics/, and /reset/

--- main.go
+++ main.go
@@ -1,16 +1,53 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) middlewareMetrics() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		views := fmt.Sprintf("Hits: %d\n", cfg.fileServerHits.Load())
+		w.Write([]byte(views))
+	})
+}
+
+func (cfg *apiConfig) resetMetrics() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		prev := cfg.fileServerHits.Swap(0)
+		current := cfg.fileServerHits.Load()
+		resetMsg := fmt.Sprintf("Metrics counter reset.\nPrevious count: %d\nNew count:%d\n", prev, current)
+		w.Write([]byte(resetMsg))
+	})
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
+	apiCfg := apiConfig{}
+
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	fileServer := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(fileServer))
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.Handle("/metrics/", apiCfg.middlewareMetrics())
+	mux.Handle("/reset/", apiCfg.resetMetrics())
 
 	s := &http.Server{
 		Handler: mux,
@@ -19,4 +56,10 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(s.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
 }
