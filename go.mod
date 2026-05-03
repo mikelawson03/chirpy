ddc1a59c59c6668608f45abf715c98ac9e190dae
Mike Lawson <mikelawson03@gmail.com>
2026-05-03 12:37:44 -0400

    build basic fileserver

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mikelawson03/chirpy
+
+go 1.26.1
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+
+	s := &http.Server{
+		Handler: mux,
+		Addr:    ":" + port,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(s.ListenAndServe())
+}
